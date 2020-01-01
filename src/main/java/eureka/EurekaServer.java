55905a8a5fc319575d8cb4b88498e53b9553b21a
jingjianqian <18697998680@163.com>
2020-01-01 15:15:00 +0800

    'autoCommit'

--- src/main/java/eureka/EurekaServer.java
+++ src/main/java/eureka/EurekaServer.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+package eureka;
 
-public class eurekaServer {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
 }
