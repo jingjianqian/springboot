3a03cf8d926fa95c00b187e29d7a5a6a0b49fa5d
jing.jianqian <529463245@qq.com>
2020-11-04 08:48:52 +0800

    Springcloud-neffix-eureka-server

---
+++ springcloud001/src/main/java/jing/eureka/Demo001EurekaServer.java
@@ -0,0 +1,13 @@
+package jing.eureka;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Demo001EurekaServer {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/jing/spring/boot/ConsumingRestApplication.java
+++ src/main/java/jing/spring/boot/ConsumingRestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsumingRestApplication {
+class ConsumingRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumingRestApplication.class, args);
